cd504f1b69ee10d24fc0592bac467d9415eeae28
Lucas Daniel <lucasdanielbarradas@gmail.com>
2024-07-31 00:59:28 -0300

    Remove debug message in healthcheck webhook route

--- app/webhooks/__init__.py
+++ app/webhooks/__init__.py
@@ -13,18 +13,14 @@ from . import twitch
 def handle_webhook_api(config: AppConfig) -> None:
     if not config.run_local_webhook_api():
         return
-    
+
     webhook_api = create_api(config.ENVIRONMENT)
     run_api_lambda = lambda: run_api(webhook_api, 5000)
 
     api_thread = Thread(target=run_api_lambda)
     return api_thread.start()
-    
+
 
 @webhooks.route('/healthcheck')
 def healthcheck():
-    from app import bot
-
-    channel = bot.get_channel(bot.config.ADMIN_LOGS_CHANNEL_ID)
-    bot.loop.create_task(channel.send('Webhooks are up and running!'))
     return 'Webhooks are up and running!', 200
